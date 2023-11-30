510d9aa41d1109bec21715162fcfe8afc94bbe1e
eli-ouaknin <147715974+Eli053@users.noreply.github.com>
2023-11-30 01:28:24 +0100

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-
 import time
-import time
-
 import threading
 
 class Horloge:
@@ -9,57 +6,41 @@ class Horloge:
         self.heure = 0
         self.minute = 0
         self.seconde = 0
-        self.pause = False
+        self.alarme = None
         self.thread = threading.Thread(target=self.incrementer_temps)
 
     def incrementer_temps(self):
         while True:
-            if not self.pause:
-                self.seconde += 1
-                if self.seconde == 60:
-                    self.seconde = 0
-                    self.minute += 1
-                if self.minute == 60:
-                    self.minute = 0
-                    self.heure += 1
-                if self.heure == 24:
-                    self.heure = 0
+            if self.seconde == 60:
+                self.seconde = 0
+                self.minute += 1
+            if self.minute == 60:
+                self.minute = 0
+                self.heure += 1
+            if self.heure == 24:
+                self.heure = 0
+            self.seconde += 1
+            self.afficher_heure()
+            if self.alarme is not None and (self.heure, self.minute, self.seconde) == self.alarme:
+                print("Alarme !")
             time.sleep(1)
 
-    def demarrer(self):
-        self.pause = False
-        if not self.thread.is_alive():
-            self.thread.start()
+    def afficher_heure(self):
+        print("{:02d}:{:02d}:{:02d}".format(self.heure, self.minute, self.seconde))
 
-    def mettre_en_pause(self):
-        self.pause = True
+    def regler_heure(self, h, m, s):
+        self.heure = h
+        self.minute = m
+        self.seconde = s
+
+    def regler_alarme(self, h, m, s):
+        self.alarme = (h, m, s)
+
+    def demarrer(self):
+        self.thread.start()
 
 # Test de la classe Horloge
 horloge = Horloge()
+horloge.regler_heure(16, 30, 0)
+horloge.regler_alarme(16, 30, 10)
 horloge.demarrer()
-time.sleep(5)  # Attendre 5 secondes
-horloge.mettre_en_pause()
-print(horloge.heure, horloge.minute, horloge.seconde)  # Affiche 0 0 5
-
-class Horloge:
-    def __init__(self):
-        self.heure_actuelle = (0, 0, 0)
-        self.alarme = None
-
-    def afficher_heure(self, heure):
-        self.heure_actuelle = heure
-
-    def regler_alarme(self, heure):
-        self.alarme = heure
-
-    def demarrer(self):
-        while True:
-            print(f"{self.heure_actuelle[0]:02}:{self.heure_actuelle[1]:02}:{self.heure_actuelle[2]:02}")
-            if self.heure_actuelle == self.alarme:
-                print("L'alarme sonne !")
-            time.sleep(1)
-            self.heure_actuelle = (self.heure_actuelle[0], self.heure_actuelle[1], (self.heure_actuelle[2] + 1) % 60)
-            if self.heure_actuelle[2] == 0:
-                self.heure_actuelle = (self.heure_actuelle[0], (self.heure_actuelle[1] + 1) % 60, self.heure_actuelle[2])
-            if self.heure_actuelle[1] == 0 and self.heure_actuelle[2] == 0:
-                self.heure_actuelle = ((self.heure_actuelle[0] + 1) % 24, self.heure_actuelle[1], self.heure_actuelle[2])
